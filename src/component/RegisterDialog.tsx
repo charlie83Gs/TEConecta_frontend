2e89fa6b213f709d9352ab286a0d273290eb8abb
jusola2 <juanjosesolano@hotmail.com>
2020-01-19 18:35:48 -0600

    Update RegisterDialog.tsx
    Se agrego el feedback al registro

--- src/component/RegisterDialog.tsx
+++ src/component/RegisterDialog.tsx
@@ -4,7 +4,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Button from '@material-ui/core/Button';
-import {TextField} from '@material-ui/core';
+import {TextField, Typography} from '@material-ui/core';
 import {registerInEvent} from '../services/event.service';
 import Event from '../model/event.model'
 
@@ -14,11 +14,19 @@ type RegisterCardProps = {
     handleAsistanceClose : Function
   }
   
-  const RegisterDialog = ({event,assitanceOpen,handleAsistanceClose} : RegisterCardProps) =>{
+const RegisterDialog = ({event,assitanceOpen,handleAsistanceClose} : RegisterCardProps) =>{
     const [name, setName] = React.useState("");
     const [ident, setId] = React.useState("");
     const [email, setEmail] = React.useState("");
-    
+    const [open, setOpen] = React.useState(false);
+       
+        const handleClickOpen = () => {
+          setOpen(true);
+        };
+      
+        const handleClose = () => {
+          setOpen(false);
+        };
   
     return (
       <Dialog
@@ -51,9 +59,28 @@ type RegisterCardProps = {
           />
           </DialogContent>
           <DialogActions className="mr-4">
-          <Button onClick={() => {handleAsistanceClose(); registerInEvent(event,name,ident,email,()=>{console.log("done")})}} color="primary" autoFocus> 
+          <Button onClick={() => {registerInEvent(event,name,ident,email,()=>{console.log("done")});handleClickOpen();}} color="primary" autoFocus> 
             Registrarse
           </Button>
+          <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="infp-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">{"Registro"}</DialogTitle>
+              <DialogContent>
+              <Typography className="mt-0 pl-2 pr-2" variant="body2" color="textSecondary"  
+                style={{whiteSpace:"pre-line", textAlign :"center"}}>
+                {"Se realizo exitosamente el registro a la actividad."}
+                </Typography>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={() => {handleAsistanceClose()}} color="primary" autoFocus>
+                  Volver
+                </Button>
+              </DialogActions>
+            </Dialog>
           <Button onClick={() => {handleAsistanceClose();}} color="primary" autoFocus> 
             Cancelar
           </Button>
